bcd4933811bb1717a288117aceeda623fc231e34
liuyixin-louis <zxcxzcz@qq.com>
2020-09-19 19:32:25 +0800

    '919'

---
+++ algorithm/dfs/checkbox.py
@@ -0,0 +1,84 @@
+class Node:
+    def __init__(self,id,father,children):
+        self.id = id
+        self.father = father
+        self.children = children
+        self.checked = False
+
+    def _print_id(self):
+        print (self.id)
+
+class CheckBox:
+    def __init__(self, init_map):
+      self.map = init_map
+      self.nodes = {}
+      self._create_tree()
+
+    def _create_tree(self):
+        for k,v in self.map.items():
+            node = Node(k,v['father'],v['children'])
+            self.nodes[k] = node
+    
+    def check_node(self,ni):
+        assert self.nodes
+        ni  = str(ni) if type(ni) != str else ni
+        node = self.nodes[ni]
+        node.checked = True # check this
+        father = node.father
+        if father==None or self._check_upward_same_layer_not_all_checked_condition(node):
+            pass
+        else:
+            self._upward(node)
+        
+        if node.children != None:
+            self._pass_down(node)
+        
+        
+    def _upward(self,node):
+        # father node
+        assert node.father
+        father_node = self.nodes[node.father]
+        father_node.checked = True
+        if father_node.father == None or self._check_upward_same_layer_not_all_checked_condition(father_node):
+            pass
+        else:
+            _upward(self,father_node.father)
+
+    def _pass_down(self,node):
+        pass
+    
+    def _check_upward_same_layer_not_all_checked_condition(self,node):
+        father_id = node.father # not None
+        assert  father_id
+        children_id_list = self.nodes[father_id].children
+        for child in children_id_list:
+            nodei = self.nodes[child]
+            if nodei.checked == False:
+                return True
+        return False
+    
+    def _reset(self):
+        for k,v in self.nodes.items():
+            v.checked = False
+
+if __name__ == "__main__":
+    demo_tree =  {
+    "1":{'father':None,'children':['2','3']},
+    "2":{'father':'1','children':['4','5']},
+    "3":{'father':'1','children':None},
+    "4":{'father':'2','children':None},
+    "5":{'father':'2','children':['6']},
+    "4":{'father':'2','children':None},
+    "6":{'father':'5','children':None}
+    }
+
+    checkbox = CheckBox(demo_tree)
+    print (1)
+
+    # test case
+    checkbox.check_node(6)
+    checkbox.check_node(4)
+    
+    assert checkbox.nodes['1'].checked == False and checkbox.nodes['2'].checked == True
+    checkbox._reset()
+
---
+++ basic/parseArgs/demo1.py
@@ -0,0 +1,23 @@
+import argparse
+
+if __name__ == "__main__":
+    # title - 输出帮助的子解析器分组的标题；如果提供了描述则默认为 "subcommands"，否则使用位置参数的标题
+    # description - 输出帮助中对子解析器的描述，默认为 None
+    # prog - 将与子命令帮助一同显示的用法信息，默认为程序名称和子解析器参数之前的任何位置参数。
+    # parser_class - 将被用于创建子解析器实例的类，默认为当前解析器类（例如 ArgumentParser）
+    # action - 当此参数在命令行中出现时要执行动作的基本类型
+    # dest - 将被用于保存子命令名称的属性名；默认为 None 即不保存任何值
+    # required - 是否必须要提供子命令，默认为 False (在 3.7 中新增)
+    # help - 在输出帮助中的子解析器分组帮助信息，默认为 None
+    # metavar - 帮助信息中表示可用子命令的字符串；默认为 None 并以 {cmd1, cmd2, ..} 的形式表示子命令
+
+    parser = argparse.ArgumentParser(description="this is a description")
+    parser.add_argument('--argu1',
+    type=str,
+    metavar='ar1', #help information
+    dest = 'a1',#the varibale name of argu1
+    help='help information of argu1')
+    args = parser.parse_args() #将参数字符串转换为对象并将其设为命名空间的属性。 返回带有成员的命名空间。
+    print(args.a1)
+
+
